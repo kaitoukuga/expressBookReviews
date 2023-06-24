89d3d1d669fd2ba9cce7f18d8679f0840a5d9b31
kaitoukuga <greaper747@gmail.com>
2023-06-24 04:34:54 -0400

    added authorization and async function code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   const username = req.body.username;
@@ -82,6 +81,82 @@ public_users.get('/review/:isbn',function (req, res) {
   } else {
       return res.status(404).send("Book not found!");
   }
+});
+
+// Async Function
+
+const axios = require('axios')
+
+// Get the book list available in the shop
+public_users.get('/', async function (req, res) {
+  //Write your code here
+  try {
+    const response = await axios.get('http://localhost:5000/');
+    const booksData = response.data;
+  
+    return res.send(JSON.stringify(booksData,null,4));
+  } catch (error) {
+    console.error('Error retrieving book data:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+  
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+//Write your code here
+  const isbn = req.params.isbn
+  axios.get(`http://localhost:5000/isbn/${isbn}`)
+    .then(response => {
+      return res.send(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).send("Error retrieving book details.")
+    })
+});
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const result = [];
+  
+    axios.get(`http://localhost:5000/author/${author}`)
+      .then(response => {
+        const booksByAuthor = response.data;
+        for (const key in booksByAuthor) {
+          if (booksByAuthor.hasOwnProperty(key)) {
+            const book = booksByAuthor[key];
+            result.push(book);
+          }
+        }
+        return res.send(result);
+      })
+      .catch(error => {
+        console.error(error);
+        return res.status(500).send("Error retrieving book details.");
+      });
+});
+
+// Get book details based on title
+public_users.get('/title/:title', async function (req, res) {
+    //Write your code here
+    const title = req.params.title;
+    const result = [];
+  
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        const booksByTitle = response.data;
+        for (const key in booksByTitle) {
+            if (booksByTitle.hasOwnProperty(key)) {
+                const book = booksByTitle[key]
+                result.push(book)
+                }
+            }
+        return res.send(result);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Error retrieving book details.");
+    }
 });
 
 module.exports.general = public_users;
